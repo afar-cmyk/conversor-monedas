02ea30394044d74c4cfe9a32c70f8fed55bb448d
Andrés F. Alvarez <andresfelipe.alvarez03@gmail.com>
2023-08-17 01:33:15 -0500

    Añadir interfaz grafica a la clase Main

--- src/main/java/com/afar/conversor/Main.java
+++ src/main/java/com/afar/conversor/Main.java
@@ -1,24 +1,33 @@
 package com.afar.conversor;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TabPane;
+import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
+	private Stage primaryStage;
+	private TabPane mainLayout;
+	
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws IOException {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Conversor Universal");
+		showMainView();
 	}
 	
+	private void showMainView() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("view/MainView.fxml"));
+		mainLayout = loader.load();
+		Scene scene = new Scene(mainLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
